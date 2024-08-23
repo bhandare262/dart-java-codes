bb14467de6497cf6271da4fd9fc28ac26a968aed
bhandare262 <pornimabhandare9@gmail.com>
2024-08-24 01:08:38 +0530

    Initial commit or description of changes

---
+++ prg5.dart
@@ -0,0 +1,14 @@
+import "dart:io";
+void main(){
+ print("enter rows");
+ int rows = int.parse(stdin.readLineSync()!);
+int number = rows;
+  for(int i =0; i<rows;i++){
+   for(int j=0; j<rows;j++){
+            stdout.write("$number ");
+}
+
+  print( "");
+number--;
+}
+}
